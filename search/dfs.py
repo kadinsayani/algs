9dd1745807850ee501edfe419da05f3d3719310e
Kadin Sayani <kadin.sayani@proton.me>
2023-02-07 11:39:12 -0700

    init commit

---
+++ search/dfs.py
@@ -0,0 +1,35 @@
+graph = {
+    'A': ['B', 'G'],
+    'B': ['C', 'D', 'E'],
+    'C': [],
+    'D': [],
+    'E': ['F'],
+    'F': [],
+    'G': ['H'],
+    'H': ['I'],
+    'I': [],
+}
+
+
+def dfs(graph, node):
+    visited = []
+    stack = []
+
+    visited.append(node)
+    stack.append(node)
+
+    while stack:
+        s = stack.pop()
+        print(s, end=" ")
+
+        for n in reversed(graph[s]):
+            if n not in visited:
+                visited.append(n)
+                stack.append(n)
+
+
+def main():
+    dfs(graph, 'A')
+
+
+main()
